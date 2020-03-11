3efc2bf4dfed5da6b1a905bfd64de3f6f001bead
jameshennigan <james@thehennigans.co.uk>
2020-03-11 20:00:15 +0000

    exports in export * as demo now individually export instead of being exported as a single object

--- lib/exportAllAs/demo.ts
+++ lib/exportAllAs/demo.ts
@@ -1,18 +1,12 @@
 
-async function func1(){
+export async function func1(){
     console.log('this is func 1');
 }
 
-async function func2(){
+export async function func2(){
     console.log('this is func 2');
 }
 
-const variable1 = 'this is var 1'
-const variable2 = 'this is var 2'
+export const variable1 = 'this is var 1'
+export const variable2 = 'this is var 2'
 
-export{
-    func1,
-    func2,
-    variable1,
-    variable2
-}
